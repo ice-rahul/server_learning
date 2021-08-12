ba392c2fa6465de2ad3d96c1eb3bc2501f15ec8b
Rahul Agrawal <meet4g@gmail.com>
2021-08-12 09:32:35 +0530

    updated end points

--- index.js
+++ index.js
@@ -1,31 +1,57 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const sequelize = require('./db/db.js');
+const express = require("express");
+const dotenv = require("dotenv");
+const sequelize = require("./db/db.js");
 const initModels = require("./models/init-models.js");
 var models = initModels(sequelize);
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
 
-app.get('/palak', async (req, res) => {
-  /*
+app.get("/getData", async (req, res) => {
+  return res.json(await models.user.findAll());
+});
+
+app.get("/addData", async (req, res) => {
   const status = await models.user.create({
-    "name": req.query.name,
-    "email": req.query.email,
-    "mobile": req.query.mobile
-  })
-  res.json(status)
-  */
- res.json(await models.user.findAll())
-})
+    name: req.query.name,
+    email: req.query.email,
+    mobile: req.query.mobile,
+  });
+  res.json(status);
+});
+
+app.get("/getDataById", async (req, res) => {
+  return res.json(
+    await models.user.findAll({
+      where: {
+        id: req.query.id,
+      },
+    })
+  );
+});
+
+app.get("/updateDataById", async (req, res) => {
+  const id = req.query.id;
+  const data = await models.user.update(
+    {
+      email: req.query.email,
+    },
+    {
+      where: {
+        id: id,
+      },
+    }
+  );
+  return res.json(data);
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server running on : http://localhost:${port}`)
-})
+  console.log(`Server running on : http://localhost:${port}`);
+});
